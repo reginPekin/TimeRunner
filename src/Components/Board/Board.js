06cf552124958c60ea3043051146f7718e7b5234
reginPekin <reginbegin@gmail.com>
2019-04-22 13:41:55 +0300

    refactoring

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -2,14 +2,59 @@ import React from "react";
 import {Task} from "../Task/index"
 import styles from "./Board.module.css"
 
+const startTime = "April 22, 2019 23:39:00"
+const finishTime = "April 27, 2019 23:39:00"
 
-export const Board = (props) => {
-    return (
-    <article className = {styles.board}>
-            {props.tasks.map(task => {
-                return <Task task={task} />;
-            })}
-       
-    </article>
+export class Board extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        tasks: this.props.tasks,
+        value: ""
+      }
+  }
+
+  onChange = (event)  => this.setState({value: event.target.value})
+
+  onSubmit = (event) => {
+    console.log(event.pageX)
+    event.preventDefault() //не обновляй-ка
+    const newTask = {
+      id: this.state.tasks.length + 1,
+      name: this.state.value,
+      startTime,
+      finishTime
+    }
+    this.setState({tasks: [ ...this.state.tasks, newTask]})
+  }
+
+  onDelete = (id) => 
+    this.setState({tasks: this.state.tasks.filter(task => task.id !== id)})
+
+
+  render() {
+
+    return(
+      <div>
+        <article className = {styles.board}>
+          {this.state.tasks.map((task, key) => {
+              return <Task 
+                key={key} 
+                task={task}
+                onDelete={() => this.onDelete(task.id)} 
+              />;
+          })}
+        </article>
+        <form >
+          <input 
+            type="text" 
+            placeholder="Your next task"
+            value = {this.state.value}
+            onChange={event => this.onChange(event)}
+          />
+          <button onClick={event => this.onSubmit(event)} >Push</button>
+        </form>
+      </div>
     );
+  }
 }
