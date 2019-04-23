d73164a6f6c91a8525e939000cc5c521e1477d43
reginPekin <reginbegin@gmail.com>
2019-04-23 17:59:56 +0300

    minDate

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -2,28 +2,43 @@ import React from "react";
 import {Task} from "../Task/index"
 import styles from "./Board.module.css"
 
-const startTime = "April 22, 2019 23:39:00"
 const finishTime = "April 27, 2019 23:39:00"
+
+let minDate = ""
+let month = ""
+let day = ""
+if(new Date().getMonth() < 9) month = "0" + (new Date().getMonth()+1)
+else month = (new Date().getMonth()+1)
+
+if(new Date().getDate() < 10) day = "0" + (new Date().getDate())
+else day = (new Date().getDate())
+
+minDate = (new Date().getFullYear() + "-" + month + "-" + day) 
+
 
 export class Board extends React.Component {
   constructor(props) {
       super(props);
       this.state = {
         tasks: this.props.tasks,
-        value: ""
+        value:"",
+        date:""
+       
       }
   }
 
   onChange = (event)  => this.setState({value: event.target.value})
+  onDateChange = (event)  => this.setState({date: event.target.value})
 
   onSubmit = (event) => {
-    console.log(event.pageX)
     event.preventDefault() //не обновляй-ка
+    console.log(this.state.date)
+    
     const newTask = {
       id: this.state.tasks.length + 1,
       name: this.state.value,
-      startTime,
-      finishTime
+      startTime: finishTime,
+      finishTime: this.state.date
     }
     this.setState({tasks: [ ...this.state.tasks, newTask]})
   }
@@ -47,10 +62,17 @@ export class Board extends React.Component {
         </article>
         <form >
           <input 
+            className = {styles.task_name}
             type="text" 
             placeholder="Your next task"
             value = {this.state.value}
             onChange={event => this.onChange(event)}
+          />
+          <input
+            type="date"
+            min = {minDate}
+            date={this.state.date} 
+            onChange={event => this.onDateChange(event)}
           />
           <button onClick={event => this.onSubmit(event)} >Push</button>
         </form>
