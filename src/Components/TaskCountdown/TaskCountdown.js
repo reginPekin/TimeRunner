17c7f7f72120896adfedadf70e4c1b6c3be9a5fa
reginPekin <reginbegin@gmail.com>
2019-04-23 18:00:35 +0300

    small change

--- src/Components/TaskCountdown/TaskCountdown.js
+++ src/Components/TaskCountdown/TaskCountdown.js
@@ -3,7 +3,7 @@ import styles from "./TaskCountdown.module.css"
 
 export const TaskCountdown = (props) => {
     return(
-        <div className={styles.task_countdown} id={props.task.idCountdown}>
+        <div className={styles.task_countdown} data-title={"start time: " + props.task.startTime + " " +  "finish time: " + props.task.finishTime}>
          {/* empty space */} в
         </div>
     );
