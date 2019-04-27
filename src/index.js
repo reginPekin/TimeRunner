707226ebe3c6a5886f12e550117a116bfbbf206f
reginPekin <reginbegin@gmail.com>
2019-04-27 20:20:57 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ const tasks = [
     {
         id:1,
         name: "sketch",
-        startTime: "April 22, 2019 23:39:00",
-        finishTime: "April 27, 2019 23:39:00"
+        startTime: "April 27, 2019 23:39:00",
+        finishTime: "April 29, 2019 23:39:00"
     },
     {
         id:2,
         name: "line",
-        startTime: "April 27, 2019 23:39:00",
+        startTime: "April 29, 2019 23:39:00",
         finishTime: "May 2, 2019 23:39:00"
     },
     {
