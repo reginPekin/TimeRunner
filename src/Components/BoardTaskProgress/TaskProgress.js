3080411098b2b8e726bca859db5e30ed5654970f
reginPekin <reginbegin@gmail.com>
2019-04-27 20:23:11 +0300

    TaskProgress green line dynamic

--- src/Components/BoardTaskProgress/TaskProgress.js
+++ src/Components/BoardTaskProgress/TaskProgress.js
@@ -1,10 +1,49 @@
 import React from "react";
 import styles from "./TaskProgress.module.css";
 
-export const TaskProgress = (props) => {
-    return(
-        <div className={styles.task_progress} id={props.task.id}>
-         {/* empty space */} 
+
+export class TaskProgress extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state= {
+            widthMy: "100%",
+            finishTime: this.props.finishTime,
+            startTime: this.props.startTime
+            };
+
+        
+    }
+    componentDidMount() {
+        this.timerID = setInterval(
+          () => this.widthChange(),
+          1000
+        );
+      }
+    
+      componentWillUnmount() {
+        clearInterval(this.timerID);
+      }
+
+      widthChange() {
+        this.setState( () => ({
+          widthMy: (100/
+          (
+            (new Date(this.state.finishTime).getTime() - new Date(this.state.startTime).getTime())/ 
+            (new Date(this.state.finishTime).getTime() - new Date().getTime())
+            )).toString() + "%"
+        }));
+      }
+    
+      render() {
+        return (
+        <div className={styles.task_progress} style={{width: this.state.widthMy}}>
+        {/* empty space */} 
         </div>
-    );
-}
+        );
+      }
+    }
+    
+
+
+
