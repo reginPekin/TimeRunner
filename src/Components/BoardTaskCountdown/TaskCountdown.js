7076158b1ba2b7930b451ecad5eff9d13e7b3329
reginPekin <reginbegin@gmail.com>
2019-04-29 22:34:13 +0300

    time countdown class

--- src/Components/BoardTaskCountdown/TaskCountdown.js
+++ src/Components/BoardTaskCountdown/TaskCountdown.js
@@ -1,14 +1,45 @@
 import React from "react";
 import styles from "./TaskCountdown.module.css"
 
-export const TaskCountdown = (props) => {
-    return(
-        <div className={styles.task_countdown} 
+
+export class TaskCountdown extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            finishTime: new Date(props.task.finishTime).getTime(),
+            startTime: new Date(props.task.startTime).getTime(),
+            date: new Date(props.task.finishTime).getTime() - new Date().getTime()
+        }
+    
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+          () => this.tick(),
+          1000
+        );
+      }
+    
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: this.state.finishTime - new Date().getTime()
+        })
+    }
+
+    render() {
+        return (
+            <div className={styles.task_countdown} 
             data-title={
-                "start time: " + props.task.startTime + 
-                " finish time: " + props.task.finishTime
-                }>
-            number
-        </div>
-    );
+                "start time: " + this.state.startTime + 
+                " finish time: " + this.state.finishTime
+                }
+                >
+            {this.state.date}
+            </div>
+        )
+    }
 }
