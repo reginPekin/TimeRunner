e4d252bb317b57a576eab2a59c2d8a90d7a30535
reginPekin <reginbegin@gmail.com>
2019-04-30 00:06:30 +0300

    more conditin

--- src/Utils/dateCountDown.js
+++ src/Utils/dateCountDown.js
@@ -2,8 +2,14 @@ export const dateCountDown = (date) => {
   const sec = Math.floor(date/1000)
   const min = Math.floor(sec/60)
   const hour = Math.floor(min/60)
+  let minScreen = min - hour * 60
+  let secScreen = sec - min * 60
 
-  return(
-    hour + ":" + (min - hour * 60) + ":" + (sec - min * 60)
-  )
+  if (minScreen < 10) minScreen = "0" + minScreen.toString();
+  if (secScreen < 10) secScreen = "0" + secScreen.toString();
+
+  if (hour < 0) return ("00:00:00")
+  else return (hour + ":" + minScreen + ":" + secScreen)
+
+ 
 }
--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ const tasks = [
         id:1,
         name: "sketch",
         startTime: "April 26, 2019 23:39:00",
-        finishTime: "April 29, 2019 23:39:00"
+        finishTime: "April 29, 2019 23:57:00"
     },
     {
         id:2,
         name: "line",
         startTime: "April 28, 2019 23:39:00",
-        finishTime: "May 2, 2019 23:39:00"
+        finishTime: "May 2, 2019 23:08:60"
     },
     {
         id:3,
