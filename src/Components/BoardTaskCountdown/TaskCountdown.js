2079f87f1087061ed3edf141161ce91f63055de8
reginPekin <reginbegin@gmail.com>
2019-05-01 22:40:44 +0300

    currentTime

--- src/Components/BoardTaskCountdown/TaskCountdown.js
+++ src/Components/BoardTaskCountdown/TaskCountdown.js
@@ -3,44 +3,53 @@ import styles from "./TaskCountdown.module.css"
 import {dateCountDown} from "../../Utils/dateCountDown"
 
 
-export class TaskCountdown extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            finishTime: new Date(props.task.finishTime),
-            startTime: new Date(props.task.startTime),
-            date: new Date(props.task.finishTime) - new Date()
-        }
-    
-    }
-
-    componentDidMount() {
-        this.timerID = setInterval(
-          () => this.tick(),
-          1000
-        );
-      }
-    
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
-
-    tick() {
-        this.setState({
-            date: this.state.finishTime - new Date()
-        })
-    }
-
-    render() {
-        return (
-            <div className={styles.task_countdown} 
-            data-title={
-                "start time: " + this.state.startTime + 
-                " finish time: " + this.state.finishTime
-                }
-                >
-            {dateCountDown(this.state.date, this.state.startTime.getTime())}
-            </div>
-        )
-    }
+export const TaskCountdown = (props) => {
+    return(
+        <div className={styles.taskCountdown}>
+            {dateCountDown
+            (
+                new Date(props.startTime).getTime(), 
+                new Date(props.finishTime).getTime(),
+                props.currentTime
+            )
+            }
+        </div>
+    )
 }
+// export class TaskCountdown extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             finishTime: new Date(props.task.finishTime),
+//             startTime: new Date(props.task.startTime),
+//         }
+    
+//     }
+
+//     componentDidMount() {
+//         this.timerID = setInterval(
+//           () => this.tick(),
+//           1000
+//         );
+//       }
+    
+
+//     tick() {
+//         this.setState({
+//             date: this.state.finishTime - new Date()
+//         })
+//     }
+
+//     render() {
+//         return (
+//             <div className={styles.task_countdown} 
+//             data-title={
+//                 "start time: " + this.state.startTime + 
+//                 " finish time: " + this.state.finishTime
+//                 }
+//                 >
+//             {dateCountDown(this.state.date, this.state.startTime.getTime())}
+//             </div>
+//         )
+//     }
+// }
