9cb5ff9a72ce3d44498c160161e41af2ec94a551
reginPekin <reginbegin@gmail.com>
2019-05-05 00:22:27 +0300

    пока caption побудет в другом месте

--- src/Components/Container/Container.js
+++ src/Components/Container/Container.js
@@ -6,7 +6,7 @@ import styles from "./Container.module.css"
 export const Container = (props) => {
   return (
     <div className={styles.container}>
-      <Caption task={props.tasks}/>
+      {/* <Caption task={props.tasks}/> */}
       <Board tasks={props.tasks}/>
     </div>
   );
