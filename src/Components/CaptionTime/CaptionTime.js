df3eb51ba72f141d94fbab8be77aa88ee6cb2283
reginPekin <reginbegin@gmail.com>
2019-05-10 23:47:21 +0300

    function for all green line componets

--- src/Components/CaptionTime/CaptionTime.js
+++ src/Components/CaptionTime/CaptionTime.js
@@ -1,13 +1,20 @@
 import React from "react";
-import styles from "./CaptionTime.module.css";
-import {myWidth} from "../../Utils/myWidth"
 
-export const CaptionTime = (props) => {
-  return (
+import styles from "./CaptionTime.module.css";
+
+import { myWidth } from "../../Utils/myWidth"
+
+export const CaptionTime = ({
+  startTime, 
+  finishTime, 
+  currentTime = Date.now(), 
+  color = 'green'}) =>(
     <div 
-    className = {styles.caption_time}
-    style={{width: myWidth(new Date(props.task[0].startTime).getTime(), new Date(props.task[props.task.length-1].finishTime).getTime())}}
+      className = {styles.captionTime}
+      style={{
+        width: myWidth(startTime, finishTime, currentTime),
+        backgroundColor: color === 'green' ? '#0baa9a' : 'white',
+      }}
     >
     </div>
-  );
-}
+)
